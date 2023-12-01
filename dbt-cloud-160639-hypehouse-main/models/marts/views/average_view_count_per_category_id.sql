d5acfe1ce82706cdd39c86138132e8ae2d857251
hypehousenimbus <152398271+hypehousenimbus@users.noreply.github.com>
2023-12-01 09:15:25 +0100

    Delete dbt-cloud-160639-hypehouse-main directory

--- dbt-cloud-160639-hypehouse-main/models/marts/views/average_view_count_per_category_id.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    category_id,
-    category,
-    ROUND(AVG(view_count), 2) AS avg_view_count 
-FROM {{ ref('dim_trending_youtube_videos_metrics') }}
-GROUP BY category_id, category
-
--- dbt-cloud-160639-hypehouse-main/models/marts/views/trending_videos_per_channel_title.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT
-    channel_title,
-    COUNT(video_id) AS trending_videos_count
-FROM {{ ref('dim_trending_youtube_videos_metrics') }}
-GROUP BY channel_title
